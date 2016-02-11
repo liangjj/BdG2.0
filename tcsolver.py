ccbcffe48da0830a0afeb0b506080cb540aceac6
Sam Roelants <roelants.sam@gmail.com>
2016-02-12 00:15:35 +0100

    Removed useless dos line, for now

--- tcsolver.py
+++ tcsolver.py
@@ -5,10 +5,11 @@ import numpy as np
 class TcSolver:
     ''' Class in charge of solving for Tc.
     '''
-    def __init__(self, parameters):
+    def __init__(self, parameters, hamiltonian):
         self.parameters = parameters
         self.phi = self.overlaps()
         self.F = self.weight
+        self.H = hamiltonian
 
     def weight(self, T, ksi):
         return np.tanh(ksi/c.kB/T)/ksi
@@ -17,12 +18,8 @@ class TcSolver:
         N = self.parameters.nu
         return (np.ones((N, N)) + np.identity(N))/self.parameters.shape.Lz
 
-    def energy_integration(self, fun, emin, emax):
+    def energy_integration(self, fun, emin, emax, dos):
         ne = 1000
         e = np.linspace(emin, emax, ne)
-        N = dos(e)
         return N
 
-
-def dos(e):
-    pass
