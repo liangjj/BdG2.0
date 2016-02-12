6b35a926fc867b685996dc9ea8954e7334b3e69a
Sam Roelants <roelants.sam@gmail.com>
2016-02-12 18:03:12 +0100

    Added DOS method to Hamiltonian class

--- system.py
+++ system.py
@@ -1,4 +1,4 @@
-from parameters import Parameters
+from parameters import Parameters, Shape
 from constants import *
 from tcsolver import TcSolver
 
@@ -12,17 +12,20 @@ class System:
         self.material = material
         self.shape = shape
         self.parameters = Parameters(material, shape)
-        self.solver = TcSolver(self.parameters)
+        self.hamiltonian = Hamiltonian(self.shape)
+        self.tcsolver = TcSolver(self.parameters, self.hamiltonian)
 
 
 class Hamiltonian:  # Have Hamiltonian take a Shape object?
     """ A Hamiltonian basically stores information on the spectrum and DOS.
     """
-    def __init__():
-        pass
+    def __init__(self, shape):
+        self.shape = shape
 
-    def dos(i, e):  # Correct definition of DOS?
-        return theta(e - h22m*(i+1)**2)
+    def dos(self, i, e):  # Correct definition of DOS?
+        ''' Density of states, *ABSOLUTE*, not relative to the fermi energy.
+        '''
+        return theta(e - h22m*np.pi**2*(i+1)**2/self.shape.Lz**2)
 
 
 def theta(x):
